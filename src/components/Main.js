7740bd13e0ab1d8b6cf988b16cc13114dfc43e4d
suvuu <hshakim597@gmail.com>
2022-08-27 03:27:02 -0400

    website reconstructed components

---
+++ src/components/Main.js
@@ -0,0 +1,22 @@
+import React from "react"
+
+
+export default function Main (){
+    return(
+        <>
+        <title>Svulst</title>
+        <div className="main-component">
+              <aside className="social-links">
+                <ul>
+                    <li>t</li>
+                    <li>f</li>
+                    <li>s</li>
+                    <li>y</li>
+                    <li>i</li>
+                </ul>
+            </aside>
+        </div>
+        </ >
+    )
+    
+    }
